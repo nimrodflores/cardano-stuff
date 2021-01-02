a8bb31aa89c21e366ccbbd744a89cce090dafd73
Nimrod Flores <nimrodlflores@yahoo.com>
2021-01-02 14:34:13 +0800

    added missing steps in update-node-1.24.2.sh

--- demo/scripts/update-node-1.24.2.sh
+++ demo/scripts/update-node-1.24.2.sh
@@ -8,6 +8,10 @@ sudo chmod 775 cardano-cli-1.24.2 cardano-node-1.24.2
 sudo rm -vrf /usr/local/bin/cardano-cli /usr/local/bin/cardano-node
 sudo ln /usr/local/bin/cardano-cli-1.24.2 /usr/local/bin/cardano-cli
 sudo ln /usr/local/bin/cardano-node-1.24.2 /usr/local/bin/cardano-node
+sudo chmod +x /usr/local/bin/cardano-cli /usr/local/bin/cardano-node
+
+# install possibly missing packages:
+sudo apt-get install -y tcptraceroute dos2unix
 
 # download the new node config file:
 wget https://raw.githubusercontent.com/nimrodflores/cardano-stuff/master/demo/configs/mainnet-config.json -O /home/$USER/cnode/files/mainnet-config.json
@@ -19,7 +23,7 @@ wget https://raw.githubusercontent.com/nimrodflores/cardano-stuff/master/demo/sc
 wget https://raw.githubusercontent.com/nimrodflores/cardano-stuff/master/demo/scripts/cntools/env -O /home/$USER/cnode/scripts/env
 wget https://raw.githubusercontent.com/nimrodflores/cardano-stuff/master/demo/scripts/cntools/gLiveView.sh -O /home/$USER/cnode/scripts/gLiveView.sh
 
-# replace username in path config:
+# replace `username` in path config:
 sed -i "s/username/$USER/" /home/$USER/cnode/scripts/cntools.sh
 sed -i "s/username/$USER/" /home/$USER/cnode/scripts/env
 
@@ -29,3 +33,22 @@ wget https://raw.githubusercontent.com/nimrodflores/cardano-stuff/master/demo/sc
 
 # fix file modes:
 chmod +x /home/$USER/cnode/scripts/*.sh
+chmod +x /home/$USER/cnode/scripts/cntools.library
+chmod +x /home/$USER/cnode/scripts/cntools.config
+
+# make sure line characters are converted to LF:
+dos2unix /home/$USER/cnode/scripts/cntools.config
+dos2unix /home/$USER/cnode/scripts/cntools.library
+dos2unix /home/$USER/cnode/scripts/cntools.sh
+dos2unix /home/$USER/cnode/scripts/env
+dos2unix /home/$USER/cnode/scripts/gLiveView.sh
+dos2unix /home/$USER/cnode/scripts/cnode-start.sh
+dos2unix /home/$USER/cnode/scripts/cnode-stop.sh
+
+# create /guild-db and shelley_trans_epoch if not yet there:
+if [ ! -f "/home/$USER/cnode/guild-db/shelley_trans_epoch" ]; then
+    if [ ! -d "/home/$USER/cnode/guild-db" ] then
+        mkdir -p /home/$USER/cnode/guild-db
+    fi
+    touch /home/$USER/cnode/guild-db/shelley_trans_epoch
+fi
